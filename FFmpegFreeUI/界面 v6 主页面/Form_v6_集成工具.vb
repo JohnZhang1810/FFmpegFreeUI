164400827b1f5ad9ca71e7ddf9c920838327568e
JohnZhang1810 <88818054+JohnZhang1810@users.noreply.github.com>
2026-07-09 17:17:31 +0800

    refactor: remove SP gate from integrated tools layout

--- FFmpegFreeUI/界面 v6 主页面/Form_v6_集成工具.vb
+++ FFmpegFreeUI/界面 v6 主页面/Form_v6_集成工具.vb
@@ -13,7 +13,7 @@ Public Class Form_v6_集成工具
         Me.ModernTabListControl1.Items(5).BoundControl = Form_v6_集成工具_Whisper生成字幕
         绑定选项卡(Form_v6_集成工具_Whisper生成字幕.ModernPanel1)
 
-        If SP_UnLock AndAlso 设置_v6.实例对象.窗口样式 = 2 AndAlso 设置_v6.实例对象.SP_毛玻璃模式 > 0 Then
+        If 设置_v6.实例对象.窗口样式 = 2 AndAlso 设置_v6.实例对象.SP_毛玻璃模式 > 0 Then
             Form_v6_集成工具_合并.ModernPanel1.Padding = New Padding(10, 20, 20, 20)
             Form_v6_集成工具_混流.ModernPanel1.Padding = New Padding(10, 20, 20, 20)
             Form_v6_集成工具_抽流.ModernPanel1.Padding = New Padding(10, 20, 20, 20)
@@ -22,14 +22,12 @@ Public Class Form_v6_集成工具
     End Sub
 
     Private Sub 绑定选项卡(选项卡的根面板容器 As ModernPanel)
-        If SP_UnLock Then
-            Select Case 设置_v6.实例对象.SP_毛玻璃模式
-                Case > 0
-                    选项卡的根面板容器.BackColor = Color.Transparent
-                    选项卡的根面板容器.BackColor1 = Color.Transparent
-                    选项卡的根面板容器.BackgroundSource = Me.ParentForm
-            End Select
-        End If
+        Select Case 设置_v6.实例对象.SP_毛玻璃模式
+            Case > 0
+                选项卡的根面板容器.BackColor = Color.Transparent
+                选项卡的根面板容器.BackColor1 = Color.Transparent
+                选项卡的根面板容器.BackgroundSource = Me.ParentForm
+        End Select
     End Sub
 
 End Class
