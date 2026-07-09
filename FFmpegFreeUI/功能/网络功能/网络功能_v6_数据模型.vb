b4dda4fda2edbab97c76a0f5dd56f17acdd9bbda
JohnZhang1810 <88818054+JohnZhang1810@users.noreply.github.com>
2026-07-09 17:20:12 +0800

    refactor: remove obsolete SP Agent and remote news models

--- FFmpegFreeUI/功能/网络功能/网络功能_v6_数据模型.vb
+++ FFmpegFreeUI/功能/网络功能/网络功能_v6_数据模型.vb
@@ -2,41 +2,6 @@ Imports System.Text.Json.Serialization
 
 Public Partial Class 网络功能
 
-    Public Class AgentSpEndpointInfo
-        <JsonPropertyName("display_name")>
-        Public Property DisplayName As String = ""
-
-        <JsonPropertyName("address")>
-        Public Property Address As String = ""
-
-        <JsonPropertyName("api_key")>
-        Public Property ApiKey As String = ""
-
-        <JsonPropertyName("extra_headers")>
-        Public Property ExtraHeaders As String = ""
-
-        <JsonPropertyName("extra_body")>
-        Public Property ExtraBody As String = ""
-
-        Public Function Clone() As AgentSpEndpointInfo
-            Return New AgentSpEndpointInfo With {
-                .DisplayName = If(DisplayName, ""),
-                .Address = If(Address, ""),
-                .ApiKey = If(ApiKey, ""),
-                .ExtraHeaders = If(ExtraHeaders, ""),
-                .ExtraBody = If(ExtraBody, "")
-            }
-        End Function
-    End Class
-
-    Public Class 新闻单片数据类
-        Public Property Title As String
-        Public Property TitleColor As String
-        Public Property SubTitle As String
-        Public Property Type As String
-        Public Property Body As String
-    End Class
-
     Public Class 国内镜像源数据结构
         <JsonPropertyName("status")>
         Public Property Status As String
